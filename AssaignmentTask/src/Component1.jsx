7a690621e643fb7d28ecec8e3d3eba156085c746
Deepthi Chodi <chodideepthi@gmail.com>
2024-12-10 23:33:14 +0530

    "updated-2"

--- AssaignmentTask/src/Component1.jsx
+++ AssaignmentTask/src/Component1.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
+import Items from './Items';
+import styles from './Component1.module.css'; // Import the CSS module
 
+const Component1 = () => {
+  return (
+    <div className={styles.component1}>
+      <Items />
+    </div>
+  );
+};
 
-export default function  Component1(){
-
-    return (
-        <div>
-          
-         
-        </div>
-      );
-    
-}
+export default Component1;
---
+++ AssaignmentTask/src/Items.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Items.module.css'; // Import the CSS module for Items
+
+const Items = () => {
+  return (
+    <div className={styles.items}>
+      <p>Item 1</p>
+      <p>Item 2</p>
+      <p>Item 3</p>
+    </div>
+  );
+};
+
+export default Items;
